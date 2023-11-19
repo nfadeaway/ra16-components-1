06cfd56c7e86aa4a6d35989d2972d59015e23c51
nfadeaway <farappl@gmail.com>
2023-11-19 12:20:18 +0500

    Initial commit

--- comp-1/src/components/ShopItemFunc.jsx
+++ comp-1/src/components/ShopItemFunc.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 
-const ShopItemFunc = (item) => {
+const ShopItemFunc = (props) => {
   return (
     <div className="main-content">
-      <h2>{item.item.brand}</h2>
-      <h1>{item.item.title}</h1>
-      <h3>{item.item.description}</h3>
-      <div className="description">{item.item.descriptionFull}</div>
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div className="description">{props.item.descriptionFull}</div>
       <div className="highlight-window mobile">
         <div className="highlight-overlay"></div>
       </div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">{item.item.currency}{item.item.price.toFixed(2)}</div>
+        <div className="price">{props.item.currency}{props.item.price.toFixed(2)}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
